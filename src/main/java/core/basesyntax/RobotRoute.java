0e4b77cb12f3d94f3d7fdb024a3c07dcd9d05ff5
DmytroShHub <juristdima@gmail.com>
2025-06-08 16:35:40 +0300

    implemented turnTo method

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,36 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        if (robot.getX() < toX) {
+            turnTo(robot, Direction.RIGHT);
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getX() > toX) {
+            turnTo(robot, Direction.LEFT);
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() < toY) {
+            turnTo(robot, Direction.UP);
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        }
+        if (robot.getY() > toY) {
+            turnTo(robot, Direction.DOWN);
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    public void turnTo(Robot robot, Direction neededDirection) {
+        while (robot.getDirection() != neededDirection) {
+            robot.turnRight();
+        }
     }
 }
